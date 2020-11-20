0434119cd168bfff61e96606954e2a4014d30913
blink-maker4 <bautista.sarahmae19@gmail.com>
2020-11-20 09:35:15 +0800

    Added Updated Files

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-const port = 7000;
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 9000
+
+app.set('view engine', 'ejs')
+app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({urlencoded:true}))
+
+
+app.get('/', async(req, res) => {
+    res.render('partials/header');
+})
+
+
+app.listen(port, () => {
+    console.log(`Server listening at port ${port}`)
+})
